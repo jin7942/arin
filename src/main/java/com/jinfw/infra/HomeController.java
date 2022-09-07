c34aca62bc7cf04883577412a6c4392afeb5168c
jin7942 <jinbuem.kim@kakao.com>
2022-09-07 18:55:21 +0900

    liyl

---
+++ src/main/java/com/jinfw/infra/HomeController.java
@@ -0,0 +1,41 @@
+package com.jinfw.infra;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+	
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate );
+
+		logger.info("========= Sever Start =========");
+		logger.info("Server name is arin.");
+		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("server address => localhost:8080/");
+		logger.info("========= ############ =========");
+		
+		
+		
+		return "home";
+	}
+	
+}
